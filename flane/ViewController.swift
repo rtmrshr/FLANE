20946144918a8c83f08f764786422364bed7e536
rtmrshr <ratmir.sher@gmail.com>
2023-02-22 12:02:45 +0200

    Initial Commit

---
+++ flane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flane
+//
+//  Created by  ratmir on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
